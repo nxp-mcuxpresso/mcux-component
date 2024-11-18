7669eb6132617783790f37ba7f6d0bf6b5dd6d23
Qiankun Li <qiankun.li@nxp.com>
2024-11-18 07:43:22 +0530

    [NXPG-925] Add copyright for conn_fwloader files.
    Copyright is the same as conn_fwloader/script/fw_bin2c_conv.py
    
    [Reviewers]
    Gannicus Guo, Jason Yu.
    
    Signed-off-by: Qiankun Li <qiankun.li@nxp.com>

--- conn_fwloader/script/image_tool/scripts/indentlog.py
+++ conn_fwloader/script/image_tool/scripts/indentlog.py
@@ -1,3 +1,10 @@
+#/*
+# * Copyright 2024 NXP
+# * 
+# * SPDX-License-Identifier: BSD-3-Clause
+# * The BSD-3-Clause license can be found at https://spdx.org/licenses/BSD-3-Clause.html
+# */
+
 import logging
 import textwrap
 
--- conn_fwloader/script/image_tool/scripts/ocotp.py
+++ conn_fwloader/script/image_tool/scripts/ocotp.py
@@ -1,3 +1,10 @@
+#/*
+# * Copyright 2024 NXP
+# * 
+# * SPDX-License-Identifier: BSD-3-Clause
+# * The BSD-3-Clause license can be found at https://spdx.org/licenses/BSD-3-Clause.html
+# */
+
 import collections
 import xml.etree.ElementTree as ET
 import argparse
