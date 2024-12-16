1912859cce5958194c2814dbd92d9cb2e20bd683
Albort Xue <yao.xue@nxp.com>
2024-12-16 17:23:35 +0800

    Revert "[MCUX-74706] Place customLUT into quick access data section."
    This reverts commit b349d473bf5bd26e47e53ab3ad846bd1fafd7764.

--- flash/mflash/mimxrt685/mflash_drv.c
+++ flash/mflash/mimxrt685/mflash_drv.c
@@ -67,7 +67,7 @@ static flexspi_device_config_t deviceconfig = {
 };
 #endif
 
-AT_QUICKACCESS_SECTION_DATA(static uint32_t customLUT[CUSTOM_LUT_LENGTH]) = {
+const uint32_t customLUT[CUSTOM_LUT_LENGTH] = {
 
     /*  OPI DDR read */
     [4 * NOR_CMD_LUT_SEQ_IDX_READ + 0] =
@@ -375,8 +375,13 @@ static int32_t mflash_drv_init_internal(void)
     /* Configure flash settings according to serial flash feature. */
     FLEXSPI_SetFlashConfig(MFLASH_FLEXSPI, &deviceconfig, FLASH_PORT);
 #endif
+
+    uint32_t tmpLUT[CUSTOM_LUT_LENGTH] = {0x00U};
+
+    memcpy(tmpLUT, customLUT, sizeof(tmpLUT));
+
     /* Update LUT table. */
-    FLEXSPI_UpdateLUT(MFLASH_FLEXSPI, 0, customLUT, CUSTOM_LUT_LENGTH);
+    FLEXSPI_UpdateLUT(MFLASH_FLEXSPI, 0, tmpLUT, CUSTOM_LUT_LENGTH);
 
     /* Hardware reset of external FLASH */
     do
