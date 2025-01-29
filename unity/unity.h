5a16203938e524be8f488af446c42bcd14892496
Antoine Bodin <antoine.bodin@nxp.com>
2025-01-29 11:28:20 +0100

    [HCIOT-4840][MCXW23] add k_unity_timer_manager in unity
    Signed-off-by: Antoine Bodin <antoine.bodin@nxp.com>

--- unity/unity.h
+++ unity/unity.h
@@ -192,6 +192,7 @@ enum _unity_module
     k_unity_elemu              = 177,
     k_unity_security_subsystem = 178,
     k_unity_rom_api            = 179,
+    k_unity_timer_manager      = 180,
 };
 
 #define MAKE_UNITY_NUM(unity_module, caseID) (((uint32_t)(unity_module)*10000) + (uint32_t)(caseID))
