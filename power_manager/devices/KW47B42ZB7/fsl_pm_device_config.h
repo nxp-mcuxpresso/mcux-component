68b9bb59bd2644040ef3881d115f0cb980480049
Albort Xue <yao.xue@nxp.com>
2025-08-08 14:22:17 +0800

    [MCUX-81959] [KW47B42ZB7] Updated bit width of inputId
    - Updated bit width of inputId, extend from 4 bits to 8 bits.
    
    Signed-off-by: Albort Xue <yao.xue@nxp.com>

--- power_manager/devices/KW47B42ZB7/fsl_pm_device_config.h
+++ power_manager/devices/KW47B42ZB7/fsl_pm_device_config.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021,2024 NXP
+ * Copyright 2021,2024-2025 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -16,7 +16,7 @@
 
 /*!
  * inputType: 4 bit width. Used to distinguish WUU input source type, 0 for external pins, 1 for internal modules.
- * inputId:   4 bit width. The id of WUU input.
+ * inputId:   8 bit width. The id of WUU input.
  * irqn:      8 bit width. The irq number of wuu input.
  * misc:      Misc usage, if input type is external pin, this field is used to store edge detection type.
  *            01b -- External input pin enabled with rising edge detection.
@@ -24,14 +24,14 @@
  *            11b -- External input pin enabled with any change detection.
  */
 #define PM_ENCODE_WAKEUP_SOURCE_ID(inputType, inputId, irqn, misc)                        \
-    (((inputType)&0xFUL) | (((inputId) << 4UL) & 0xF0UL) | (((irqn) << 8UL) & 0xFF00UL) | \
-     ((misc << 16UL) & 0xFFFF0000UL))
+    (((inputType)&0xFUL) | (((inputId) << 4UL) & 0xFF0UL) | (((irqn) << 12UL) & 0xFF000UL) | \
+     ((misc << 20UL) & 0xFFF00000UL))
 
 #define PM_DECODE_WAKEUP_SOURCE_ID(wsId)  \
     inputType = ((wsId)&0xFUL);           \
-    inputId   = ((wsId)&0xF0UL) >> 4UL;   \
-    irqn      = ((wsId)&0xFF00UL) >> 8UL; \
-    misc      = ((wsId)&0xFFFF0000UL) >> 16UL
+    inputId   = ((wsId)&0xFF0UL) >> 4UL;   \
+    irqn      = ((wsId)&0xFF000UL) >> 12UL; \
+    misc      = ((wsId)&0xFFF00000UL) >> 20UL
 
 #endif /* _FSL_PM_DEVICE_CONFIG_H_ */
 
