01454091b7881ace435a04649463ef2e8fed3575
Patrik Nemeth <patrik.nemeth@nxp.com>
2026-06-24 16:12:24 +0200

    [MCUX-88729][ele_hseb] Fix licenses
    Should be LA_OPT Online Code Hosting.
    
    Signed-off-by: Patrik Nemeth <patrik.nemeth@nxp.com>

--- ele_hseb/services/src/secure_boot/hse_secure_boot.c
+++ ele_hseb/services/src/secure_boot/hse_secure_boot.c
@@ -11,12 +11,7 @@
 *
 *   Copyright 2022 NXP.
 *
-*   This software is owned or controlled by NXP and may only be used strictly in accordance with
-*   the applicable license terms. By expressly accepting such terms or by downloading, installing,
-*   activating and/or otherwise using the software, you are agreeing that you have read, and that
-*   you agree to comply with and are bound by, such license terms. If you do not agree to
-*   be bound by the applicable license terms, then you may not retain, install, activate or
-*   otherwise use the software.
+*   License: LA_OPT_Online Code Hosting NXP_Software_License - v1.4 May 2025
 ==================================================================================================*/
 
 #ifdef __cplusplus
