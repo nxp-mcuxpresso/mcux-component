9ac7bef4edcb156d6f74110453be7eeddaec3762
Eric Sun <jian.sun@nxp.com>
2026-07-08 07:56:11 -0700

    [MCUX-88926][RT2660] Coremark : using ITERATIONS 0 to automatically select a proper value
    Signed-off-by: Eric Sun <jian.sun@nxp.com>

--- coremark/porting/RT2660/core_portme.h
+++ coremark/porting/RT2660/core_portme.h
@@ -30,7 +30,7 @@ Original Author: Shay Gal-on
 
 /********************************* NXP add-on **********************************/
 #ifndef ITERATIONS
-#define ITERATIONS 20000
+#define ITERATIONS 0
 #endif
 
 #define printf DbgConsole_Printf
